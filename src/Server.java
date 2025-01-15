ebfd8d21af2a4abb12282a8ec76450037cf741a3
Andy Pham <ap3879@drexel.edu>
2025-01-15 14:41:00 -0500

    init

---
+++ src/Server.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+import static java.lang.Integer.parseInt;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        try {
+            int port = parseInt(args[1]);
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.print("The user typed: ");
+                System.out.println(inputLine);
+            }
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+}
